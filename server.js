01b4e6cc9c2d1ab8d44880c6900144106382786f
macheidoHTML <matheus.machadosilv@gmail.com>
2025-02-28 23:01:16 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Rota de exemplo
+app.get('/', (req, res) => {
+  res.send('Bem-vindo ao Internimes!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
